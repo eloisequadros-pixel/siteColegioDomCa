f97c1c10f6dd923721398b8f0ea8bc771162ff90
eloisequadros-pixel <eloise.quadros@escola.pr.gov.br>
2026-04-06 08:28:41 -0300

    sal

---
+++ script.js
@@ -0,0 +1,5 @@
+Document.addtventlistener("DomContentLoaded". function() {
+    //corige o ul da agenda caso a classe nao esteja certa
+    Count secasAgenda document.querySelector9("#agenda)
+    
+});
