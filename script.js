96e0ef30b8fcb31cd06615db2fef97f8a141ddbd
eloisequadros-pixel <eloise.quadros@escola.pr.gov.br>
2026-04-06 11:04:25 -0300

    salve

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener("DomContentLoaded",function(){
+document.addEventListener("DomContentLoaded",function() {
     const secaoAgenda = document.querySelector("#agenda");
     if (secaoAgenda){
         const ulAgenda = secaoAgenda.querySelector("ul");
@@ -12,3 +12,21 @@ if(secaoContato){
     secaoContato.classList.add(contato-box);
 }
 
+//seleciona Links do menu
+const LinksMenu = document.querySelector("nav a");
+const secoes = document.querySelectorAll("section[id");
+
+//Deixa a transição suave nos cards via js
+const cards = document.querySelectorAll(".card");
+for(let i = 0; 1 < cards.length; 1++) {
+    cards[i].style.transition = "0.3s ease";
+
+    cards[i].addEventListener("mouseenter", function())
+    this.style.transform = "translateY(-6px)";
+});
+
+cards[i].addEventListener("mouseenter", function())
+    this.style.transform = "translateY(0)";
+});
+}
+});
